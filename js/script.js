a3295dfdce2cf2226db2c61cf8c5dc7ac2bcb80f
Ermygut <ermygut@gmail.com>
2021-03-09 22:12:32 +0200

    working with classes to create in dynamic menuCards

--- js/script.js
+++ js/script.js
@@ -97,53 +97,118 @@ window.addEventListener('DOMContentLoaded', () => {
 	// modal window pop-up
 
 	const modalTrigger = document.querySelectorAll('[data-modal]'), //обращение к элементу чере дата атрибут
-		  modal = document.querySelector('.modal'),
-		  modalCloseBtn = document.querySelector('[data-close]');
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]');
 
 	modalTrigger.forEach(btn => {
-		btn.addEventListener('click', openModal); 	
-	});	  
+		btn.addEventListener('click', openModal);
+	});
 
 	function openModal() {
-		modal.classList.add('show');			// добавляем стиль из ЦСС, там он прописан, меняет display block/none
+		modal.classList.add('show'); // добавляем стиль из ЦСС, там он прописан, меняет display block/none
 		modal.classList.remove('hide');
 		document.body.style.overflow = 'hidden'; //убрать прокрутку страницы за модальным окном
-		clearInterval(modalTimerId);  
+		// clearInterval(modalTimerId);  !!!!!!!!!!!!!!!!!!!!
 		// пользователь сам открыл модальное окно-должны вырубить таймер и не показывать еще раз окно через modalTimerId
 	}
 
 	function closeModal() {
 		modal.classList.add('hide');
 		modal.classList.remove('show');
-		document.body.style.overflow = ''; 		 // вернуть прокрутку страницы после закрытия модального окна
+		document.body.style.overflow = ''; // вернуть прокрутку страницы после закрытия модального окна
 
 	}
 
-	modalCloseBtn.addEventListener('click', closeModal); 
+	modalCloseBtn.addEventListener('click', closeModal);
 
 
-					// если кликаем во всю область modal и если НЕ КОНКРЕТНО в modal dialog тогда закрываем весь попап
-	modal.addEventListener('click', (e) => {       
+	// если кликаем во всю область modal и если НЕ КОНКРЕТНО в modal dialog тогда закрываем весь попап
+	modal.addEventListener('click', (e) => {
 		if (e.target === modal) {
 			closeModal();
 		}
 	});
 
-					// закрыть модальное окно при нажатии esc 
+	// закрыть модальное окно при нажатии esc 
 	document.addEventListener('keydown', (e) => {
 		if (e.code === "Escape" && modal.classList.contains('show')) {
 			closeModal();
 		}
 	});
-
-	const modalTimerId = setTimeout(openModal, 50000); // через 5 секунд показать пользователю модальное окно
+	//!!!!!!!!!!!!!!!!!!!
+	// const modalTimerId = setTimeout(openModal, 50000); // через 5 секунд показать пользователю модальное окно
 
 	function showModalByScroll() {
 		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
 			openModal();
 			window.removeEventListener('scroll', showModalByScroll);
 		}
-	} 	
+	}
 	window.addEventListener('scroll', showModalByScroll);
-				 // показывать один раз модальное окно при прокрутке до конца страницы
+	// показывать один раз модальное окно при прокрутке до конца страницы
+
+	// использование классов для карточек
+
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 27;
+			this.changeToUAH();
+		}
+
+		changeToUAH() {
+			this.price = +this.price * this.transfer;
+		}
+
+		render() {
+			const element = document.createElement('div');
+			element.innerHTML = `
+			<div class="menu__item">
+					<img src=${this.src} alt=${this.alt}>
+					<h3 class="menu__item-subtitle">${this.title}</h3>
+					<div class="menu__item-descr">${this.descr}</div>
+					<div class="menu__item-divider"></div>
+					<div class="menu__item-price">
+						<div class="menu__item-cost">Цена:</div>
+						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+					</div>
+				</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+	new MenuCard(
+		"img/tabs/vegy.jpg",
+		"vegy",
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		9,
+		'.menu .container'
+	).render();
+
+	new MenuCard(
+		"img/tabs/elite.jpg",
+		"elite",
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода	в ресторан!',
+		14,
+		'.menu .container'
+	).render();
+
+	new MenuCard(
+		"img/tabs/post.jpg",
+		"post",
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие 		продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		21,
+		'.menu .container'
+	).render();
+
+	
 });
